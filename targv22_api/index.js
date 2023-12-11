ac6a70edd59d7a566ca5cac095e330d6e24e2510
Vyacheslav Akchurin <118188091+akchurinslava@users.noreply.github.com>
2023-12-11 16:23:12 +0200

    Delete finished

--- targv22_api/index.js
+++ targv22_api/index.js
@@ -25,6 +25,10 @@ app.get('/games/:id', (req, res) =>{
     res.send(games[req.params.id - 1])
 })
 
+app.get('/games', (req, res) => {
+    res.send(games);
+});
+
 app.post('/games', (req, res) =>{
     if(!req.body.name || !req.body.price || !req.body.rating){
         return res.status(400).send({error: "One or all parameteres are missing"})
@@ -38,13 +42,21 @@ app.post('/games', (req, res) =>{
     games.push(game)
     
     res.status(201).location(`${getBaseUrl(req)}/games/${games.length}`).send(games)
-})
+});
+
+app.delete('/games/:id', (req, res) =>{
+    if(typeof games[req.params.id - 1] === 'undefined'){
+        return res.status(404).send({error: "Game not found"})
+    };
+    games.splice(req.params.id -1, 1);
+    res.status(204).send({error: "No Content"});
+});
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-app.listen(port, () => {console.log(`API up at: http://localhost:${port}`)})
+app.listen(port, () => {console.log(`API up at: http://localhost:${port}`)});
 
 function getBaseUrl(req){
     return req.connection && req.connection.encrypted ? 'https' : 'http' + `://${req.headers.host}`
-}
+};
 
