03881688ee72e90e809d3d3d0f6f0e68c6024e9c
Vyacheslav Akchurin <118188091+akchurinslava@users.noreply.github.com>
2024-01-08 17:17:01 +0200

    db part 1

--- targv22_api/index.js
+++ targv22_api/index.js
@@ -1,25 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 8080
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const port = process.env.APP_PORT;
+const mariadb = require("mariadb");
+const cors = require('cors');
 const swaggerUI = require('swagger-ui-express');
 const yamljs = require('yamljs');
 //const swaggerDocument = require('./docs/swagger.json');
 const swaggerDocument = yamljs.load('./docs/swagger.yaml');
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-const games = [
-    {id: 1, name: "Team Fortress 1", price: "free", rating: 11},
-    {id: 2, name: "Team Fortress 2", price: 2.99, rating: 24},
-    {id: 3, name: "Team Fortress 3", price: 4.32, rating: 39},
-    {id: 4, name: "Team Fortress 4", price: 6.3, rating: 44},
-    {id: 5, name: "Team Fortress 5", price: 6.32, rating: 56},
-    {id: 6, name: "Team Fortress 6", price: 6.3, rating: 61},
-    {id: 7, name: "Team Fortress 7", price: 63.3, rating: 74},
-    {id: 8, name: "Team Fortress 8", price: 6.23, rating: 82},
-    {id: 9, name: "Super Mario 9", price: 353.2, rating: 93}
-]
+const pool = mariadb.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASS,
+    database: process.env.NAME,
+    connectionLimit: 5
+})
+
+// const games = [
+//     {id: 1, name: "Team Fortress 1", price: "free", rating: 11},
+//     {id: 2, name: "Team Fortress 2", price: 2.99, rating: 24},
+//     {id: 3, name: "Team Fortress 3", price: 4.32, rating: 39},
+//     {id: 4, name: "Team Fortress 4", price: 6.3, rating: 44},
+//     {id: 5, name: "Team Fortress 5", price: 6.32, rating: 56},
+//     {id: 6, name: "Team Fortress 6", price: 6.3, rating: 61},
+//     {id: 7, name: "Team Fortress 7", price: 63.3, rating: 74},
+//     {id: 8, name: "Team Fortress 8", price: 6.23, rating: 82},
+//     {id: 9, name: "Super Mario 9", price: 353.2, rating: 93}
+// ]
 
 app.get('/games/:id', (req, res) =>{
     if(typeof games[req.params.id - 1] === 'undefined'){
@@ -28,8 +37,17 @@ app.get('/games/:id', (req, res) =>{
     res.send(games[req.params.id - 1])
 })
 
-app.get('/games', (req, res) => {
-    res.send(games);
+app.get('/games', async (req, res) => {
+    let connection
+    try{
+        connection = await pool.getConnection()
+        const rows = await connection.query("SELECT id, name FROM games")
+        res.send(rows)
+    } catch (error){
+        throw error
+    }finally{
+        if(connection) return connection.end()
+    }
 });
 
 app.post('/games', (req, res) =>{
