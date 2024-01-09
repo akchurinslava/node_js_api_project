c9be07f3888294543fd5234df1cbf1bdf822b440
Vyacheslav Akchurin <118188091+akchurinslava@users.noreply.github.com>
2024-01-09 15:28:48 +0200

    commit

---
+++ routes/app_routes.js
@@ -0,0 +1,6 @@
+const gamesController = require('../controllers/GamesController.js')
+
+module.exports = (app) => {
+    app.route("/games")
+        .get(gamesController.getAll)  // get all games as list
+}
---
+++ targv22_api/db.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+    process.env.NAME,
+    process.env.USER,
+    process.env.PASS,
+    {
+        host: process.env.HOST,
+        dialect: "mariadb",
+        define: {
+            timestamps: false
+        }
+    }
+);
+
+const db = {}
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+db.games = require("./models/Game.model")(sequelize,Sequelize)
+
+module.exports = db
