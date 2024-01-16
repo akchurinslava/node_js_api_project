8807a697ff154f258ab8e3c60c0de7e025e29e81
Vyacheslav Akchurin <118188091+akchurinslava@users.noreply.github.com>
2024-01-16 18:27:15 +0200

    As an app developer, I want to get a list of all orders to show in my app[Delivers #186852151]

--- controllers/GamesController.js
+++ controllers/GamesController.js
@@ -37,6 +37,43 @@ exports.createNew = async (req, res) => {
     res.status(201).location(`${getBaseUrl(req)}/games/${game.id}`).json(game)
 }
 
+exports.updateById = async (req, res) => {
+    let result
+    delete (req.body.id)
+    try{
+        result = await Game.update(req.body, {where: {id: req.params.id}})
+    }
+    catch(error) {
+        console.log("GameUpdate: ", error)
+        res.status(500).send({"error": "server error, try again later"})
+        return
+    }
+    if (result === 0 || result === undefined){
+        res.statsu(404).send({"error":"Game not found"})
+        return
+    }
+    const game = await Game.findByPk(res.params.id)
+    res.status(200).location(`${getBaseUrl(req)}/games/${game.id}`).json(game)
+}
+
+exports.deleteById = async (req, res) => {
+    let result;
+    try{
+        result = await Game.destroy({where: {id: req.params.id}})
+    }
+    catch (error){
+        console.log("GamesCreateDelete ", error)
+        res.status(500).send({"error": "server error, try again later"})
+        return
+        }
+        if(result === 0 || result === undefined){
+            res.status(404).send({"error": "Game not found"})
+            return
+        }
+        res.status(204).send()
+        
+    }
+
 getBaseUrl = (request) => {
     return (
         (request.connection && request.connection.encrypted ? "https": "http") +
--- routes/app_routes.js
+++ routes/app_routes.js
@@ -6,4 +6,7 @@ module.exports = (app) => {
         .post(gamesController.createNew) // Create a new game
     app.route("/games/:id")
         .get(gamesController.getById) //get a game by id
+        .put(gamesController.updateById) //change a info of a game by id
+        .delete(gamesController.deleteById) //delete a game by id
+
 }
